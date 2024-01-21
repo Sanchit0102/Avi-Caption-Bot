53e42b2e93a96ae2099d3f87225d8848b78b9f73
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2024-01-21 18:36:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,40 @@
-import pyrogram
-# pyrogram.sync_time.sleep()
 import logging
-logging.basicConfig(level=logging.WARNING,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
-import os
-
-# Access environment variable
-timezone = os.environ.get("TIMEZONE")
-
-# Use the variable in your code
-print(f"Timezone= {timezone}")
-
 from config import Config
-from pyrogram import Client 
-logging.getLogger("pyrogram").setLevel(logging.DEBUG)
+from pyrogram import Client
 
-class autocaption(Client):
-    
+class AutoCaptionBot(Client):
     def __init__(self):
         super().__init__(
-            session_name="sanchit",
-            bot_token = Config.BOT_TOKEN,
-            api_id = Config.API_ID,
-            api_hash = Config.API_HASH,
-            workers = 20,
-            plugins = dict(
-                root="Plugins"
-            )
-      )  
-    async def start(self):
-        await super().start()
-        print("Time Synchronization...")
-        await self.sync_time()
+            name="Captioner",
+            bot_token="6650529779:AAGYioPMLCR1ZPTq93DTSP_iTGDC7mATgpU",
+            api_id="16536417",
+            api_hash="f6e58a549da642d7b765744a2f82c6d9",
+            workers=20,
+            plugins=dict(root="Plugins"),
+        )
+        
+        # Enable additional logging for Pyrogram
+        self.log_verbosity = logging.INFO
 
-    async def sync_time(self):
+    def run(self):
         try:
-            await self.send(pyrogram.functions.Ping(data=bytes(8)))
-        except pyrogram.errors.RPCError as e:
-            print(f"Time Synchronization Error= {e}")
-        else:
-            print("Time Synchronization Successful")
-if __name__ == "__main__" :
-run()
+            logger.info("Bot is starting...")
+            super().run()
+            logger.info("Bot is up and running.")
+        except Exception as e:
+            logger.error(f"An error occurred during bot execution: {e}")
+        finally:
+            logger.info("Bot is shutting down.")
+
+if __name__ == "__main__":
+    AutoCaptionBot().run()
